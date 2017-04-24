03b39307e2541428bab155763f2da454d7ebb388
Delphinoid <penganddolph@gmail.com>
2017-04-24 18:10:34 +1000

    Added everything.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fred the Giant Cactus Bot
+
+This is a simple bot written with Node.js and discord.js, designed to be used in Dangevin's Platform Racing Discord server. Yes, I do realize `npm start` contains more characters than `node .`.
+
+
+
+## Commands
+
+| Command    | Description                                                         |
+| ---------- | ------------------------------------------------------------------- |
+| !ftgc_help | Sends the user a direct message describing the bot's functionality. |
---
+++ fred.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login("MzA1OTM1NDM5NTk5MTA4MDk3.C98jvA.caPOugXy3M4Yxlt7jO02LMbZVuE");
+
+// Joined the server
+client.once("ready", () => {
+	console.log("Bot up and running. Currently connected as " + client.user.username + ".");
+	client.user.setGame("!ftgc_help");
+});
+
+// New member joined
+client.on("guildMemberAdd", (member) => {
+member.guild.channels.get(member.guild.id).sendMessage("Hi " + member.toString() + ", I'm Fred the Giant Cactus. " +
+	                                                   "Welcome to the Platform Racing Discord server!\n\n" +
+	                                                   "Let me notify the staff so they can verify you. @Owner @Admins @Moderators\n" +
+	                                                   "In the mean time, please consider looking at the #rules.\n\n" +
+	                                                   "I'll be seeing you around.  :fred:");
+});
+
+// Help command posted
+client.on("message", (message) => {
+	if(message.content == "!ftgc_help"){
+		message.member.sendMessage("Sorry, until I add more functionality to this bot this message won't contain anything useful.");
+	}
+});
